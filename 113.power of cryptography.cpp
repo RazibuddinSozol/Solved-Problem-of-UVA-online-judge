141c4c85cb923e2e3d6c731949b777f5440dd0f6
Razibuddin Sozol <44525991+RazibuddinSozol@users.noreply.github.com>
2019-11-13 15:50:39 +0600

    Add files via upload

---
+++ 113.power of cryptography.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    double a,b,c,d;
+    while((scanf("%lf%lf",&a,&b))==2){
+        c=1.0/a;
+        d=pow(b,c);
+        printf("%0.0lf\n",d);
+  }
+   return 0;
+}
